7b9b0b4dbd7907c925ec467d1199426edfc3c19e
jonassvalin <jonassvalin@gmail.com>
2015-02-11 13:02:30 +0100

    Changed how to the JUnit test

--- QlikJS/src/git_commander.java
+++ QlikJS/src/git_commander.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,38 +5,43 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class git_commander {
-	
-	private String command;
-	
-	public git_commander(String path , String command){
-	
-		this.command = "cd " + "\"" + path + "\" && " + command;		
+
+	private String cmd;
+
+	public git_commander(){
+
+		//this.cmd = "cd " + "\"" + path + "\" && ";                               
 		// "cmd.exe", "/c", "cd \"C:\\git\\qliktest\" && git diff a7afc9227df4aba409e652054f0f92c992230784"
 	}
-	public ArrayList<String> run(){
-		ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", command);
-		 processBuilder.redirectErrorStream(true);
-		 Process process = null;
+	public ArrayList<String> run(String path, String command){
+		BufferedReader r = reader(path, command);
+		ArrayList result = new ArrayList<String>(); 
+		String a = "";
+		try{
+			while(true){
+				a = r.readLine();
+				if(a != null){
+					result.add(a);
+				}else{break;}
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	private BufferedReader reader(String path, String command){
+		cmd = "cd " + "\"" + path + "\" && " + command; 
+		ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", cmd);
+		processBuilder.redirectErrorStream(true);
+		Process process = null;
 		try {
 			process = processBuilder.start();
 		} catch (IOException e1) {
 			System.out.println("fel i processbuilder.start");
 			e1.printStackTrace();
 		}
-		 BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()));
-		 ArrayList result = new ArrayList<String>(); 
-		 String a = "";
-		 try{
-		 while(true){
-			 a = r.readLine();
-			 if(a != null){
-				 result.add(a);
-			 }else{break;}
-		 }
-		 }catch(IOException e){
-			 e.printStackTrace();
-		 }
-		 
-		 return result;
+		return new BufferedReader(new InputStreamReader(process.getInputStream()));
 	}
 }
--- QlikJS/src/main.java
+++ QlikJS/src/main.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
 
+import junit.framework.Test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import com.testscripts.Test3;
 
 public class main {
 
 	public static void main(String[] args) {
-		new ReadXMLFile("coverage/new.xml");
-		git_commander g = new git_commander("C:\\git\\qliktest", "git diff b1cf39e18a7110e658ffc04d8c7f834fc60357ed 450e5654b9be7115dfc590315cd8c6b0aaf35ae2 > C:\\git\\qliktest\\QlikTest\\coverage\\commits.txt");
-		g.run();
-		new AutoCompare("coverage/coverageanalysis.txt", "coverage/commits.txt");
+		git_commander g = new git_commander();
+		System.out.println("1");
+		ArrayList<String> a =  g.run("C:\\Users\\ext_jvs\\JScover\\examples\\localStorage-proxy", "sh web-server.sh");
+		ArrayList<String> b =  g.run("C:\\Users\\ext_jvs\\JScover\\examples\\localStorage-proxy", "sh jscover-proxy.sh");
+		System.out.println(a.toString());
+		System.out.println(b.toString());
+		System.out.println("2");
+		Result result = JUnitCore.runClasses(Test3.class);
+		System.out.println("3");
+		//g.run("C:\\git\\qliktest", "git diff a7afc9227df4aba409e652054f0f92c992230784 > C:\\git\\qliktest\\QlikTest\\coverage\\commits.txt");
 	}
+
 }
