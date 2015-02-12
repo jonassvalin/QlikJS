3add4f46d66b136c9a7945941fb8f07e1594aba8
axelhildingson <a.hildingson@gmail.com>
2015-02-12 15:19:15 +0100

    fixed moveremove

--- QlikJS/src/MoveRemove.java
+++ QlikJS/src/MoveRemove.java
@@ -1,4 +1,14 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class MoveRemove {
-
+	public MoveRemove(String path){
+		File f = new File(path);
+		if(f.exists()){
+			f.delete();
+		}
+	}
 }
