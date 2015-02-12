1b0445cbbe9e1f8c43ccf5c7ffd831f226a65882
jonassvalin <jonassvalin@gmail.com>
2015-02-12 14:20:39 +0100

    JSON

--- QlikJS/src/ReadJSON.java
+++ QlikJS/src/ReadJSON.java
@@ -1,18 +1,14 @@
 import java.io.File;
+import java.io.FileReader;
 import java.io.PrintWriter;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
+import org.json.simple.*;
 
 public class ReadJSON {
 	public ReadJSON(String path) {
 		try {
-			File JSON = new File(path + "jscoverage.json");
-			//System.out.println(toString(JSON));
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			String jsonOutput = gson.toJson(JSON);
-			System.out.println(jsonOutput);
+			Object obj = new FileReader(path);
+			JSONObject jsonObject = (JSONObject) obj;
+			System.out.println(jsonObject);
 			PrintWriter writer = new PrintWriter(path + "coverageanalysis.txt", "UTF-8");
 			//writer.println("The following lines were covered: ");
 			
