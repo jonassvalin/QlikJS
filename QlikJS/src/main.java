484e62b451387664bcc0f0c8d064ada743864d60
jonassvalin <jonassvalin@gmail.com>
2015-02-16 09:47:03 +0100

    Prettify the code

--- QlikJS/src/main.java
+++ QlikJS/src/main.java
@@ -8,16 +8,15 @@ import org.junit.runner.Result;
 import com.testscripts.Test3;
 
 public class main {
+	
+	private static String JONAS = "C:\\Users\\ext_jvs\\JScover";
+	private static String AXEL = "C:\\jscover\\jscover- build";
 
 	public static void main(String[] args) {
-
-		String jonas = "C:\\Users\\ext_jvs\\JScover";
-		String axel = "C:\\jscover\\jscover- build";
 		
+		String path = JONAS;
 		String pathWamp = "C:\\wamp\\www";
-		String Gitcommand = "git diff --pretty=oneline 11f91de8996b758302c39381e0425f32126eae1d";
-
-		String path = jonas;
+		String Gitcommand = "git diff --pretty=oneline --minimal 11f91de8996b758302c39381e0425f32126eae1d";
 		new MoveRemove(path + "\\target\\local-storage-proxy\\no-frames\\jscoverage.json");
 		/*
 		CmdThread webServer = new CmdThread(new Commander(), path + "\\examples\\localStorage-proxy", "sh web-server.sh");
