e3d44fc1dd76a065ff6c21e5f3933df5af82373d
axelhildingson <a.hildingson@gmail.com>
2015-02-16 14:56:57 +0100

    Cleanup

--- QlikJS/src/AutoCompare.java
+++ QlikJS/src/AutoCompare.java
@@ -1,7 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,81 +7,78 @@ import org.json.simple.JSONObject;
 
 public class AutoCompare {
 
-private Pattern p;	
-	
+	private Pattern p;
+	private JSONObject output;
+	private JSONArray currentKeyArray;
+	private String currentKey;
+	private String line;
+
 	public AutoCompare(String covPath, String comPath) {
 		File coverage = new File(covPath);
 		File commits = new File(comPath);
-		this.p = Pattern.compile("^\100\100\\s.([0-9]+).([0-9]+)\\s.([0-9]+).([0-9]+)\\s\100\100.*");
-		getGitLines(comPath);
-		
-		/*
-		 * try { Scanner scanCoverage = new Scanner(coverage); while
-		 * (scanCoverage.hasNextLine()) { String coverageLine =
-		 * scanCoverage.nextLine(); Scanner scanCommits = new Scanner(commits);
-		 * while (scanCommits.hasNextLine()) { String commitsLine =
-		 * scanCommits.nextLine(); if(commitsLine.contains(coverageLine) &&
-		 * coverageLine.length() > 3) { System.out.println("Method: " +
-		 * coverageLine); } }
-		 * 
-		 * } } catch (Exception e) { System.out.println("SCANNER ISSUE " + e);
-		 * 
-		 * }
-		 */
+		p = Pattern
+				.compile("^\100\100\\s.([0-9]+).([0-9]+)\\s.([0-9]+).([0-9]+)\\s\100\100.*");
+		getGitLines(commits);
 	}
 
-	private void getGitLines(String filePath) {
+	private void getGitLines(File filePath) {
 		try {
-		FileReader file = new FileReader(filePath);	
-		BufferedReader br = new BufferedReader(file);
-		String line;
-		int lineEnd = 0;
-		int lineCounter = 0;
-		line = br.readLine();
-		int counter = 0;
-		String currentKey = "";
-		JSONObject output = new JSONObject();
-		JSONArray currentKeyArray = null;
-
-		while (line != null) {
-			if(Pattern.matches("diff --.*", line)){
-				currentKeyArray = new JSONArray();
-				String[] Lines = line.split("/");
-				currentKey = Lines[Lines.length-1];
-				//output.put(currentKey, currentKeyArray);
-			}else if (lineRegex(line) ){
-				counter ++;
-				System.out.println(counter + " : " + line);
-				line = line.substring(line.indexOf("@@") +2 ,line.indexOf("@@",line.indexOf(" ") ) );
-				//System.out.println(line.substring(line.indexOf("+"),line.indexOf(" ",line.indexOf("+"))).replace("+",""));
-				String lineNumber[] = line.substring(line.indexOf("+"),line.indexOf(" ",line.indexOf("+"))).replace("+","").split(",");
-				lineCounter  = Integer.parseInt(lineNumber[0]);
-				lineEnd = Integer.parseInt(lineNumber[1]) + Integer.parseInt(lineNumber[0]);
-				while(lineCounter < lineEnd){
-					currentKeyArray.add(Integer.toString(lineCounter));
-					lineCounter++;
-				}
-				output.put(currentKey, currentKeyArray);
-			}
+			FileReader file = new FileReader(filePath);
+			BufferedReader br = new BufferedReader(file);
+			output = new JSONObject();
 			line = br.readLine();
-		}
-		
-		br.close();
-		PrintWriter writer = new PrintWriter("C:/wamp/www/coverageanalysis.json");
-		writer.print(output.toJSONString());
-		writer.flush();
-		writer.close();
-		}catch (IOException e) {
 
+			while (line != null) {
+				if (Pattern.matches("diff --.*", line)) {
+					newFileIdentified(line);
+				} else if (lineRegex(line)) {
+					newChangeIdentified(line);
+				}
+				line = br.readLine();
+			}
+			br.close();
+			writeToFile();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private boolean lineRegex(String line) {
-		// System.out.println(line.trim());
-		Matcher m = p.matcher(line);
-		return m.matches();
-
+	private void writeToFile() throws IOException {
+		PrintWriter writer = new PrintWriter(
+				"C:/wamp/www/coverageanalysis.json");
+		writer.print(output.toJSONString());
+		writer.flush();
+		writer.close();
 	}
 
+	private void newChangeIdentified(String line) {
+		int lineEnd = 0;
+		int lineCounter = 0;
+		line = line.substring(line.indexOf("@@") + 2,
+				line.indexOf("@@", line.indexOf(" ")));
+		String lineNumber[] = line
+				.substring(line.indexOf("+"),
+						line.indexOf(" ", line.indexOf("+"))).replace("+", "")
+				.split(",");
+		lineCounter = Integer.parseInt(lineNumber[0]);
+		lineEnd = Integer.parseInt(lineNumber[1])
+				+ Integer.parseInt(lineNumber[0]);
+		while (lineCounter < lineEnd) {
+			currentKeyArray.add(Integer.toString(lineCounter));
+			lineCounter++;
+		}
+		output.put(currentKey, currentKeyArray);
+	}
+
+	private void newFileIdentified(String line) {
+		currentKeyArray = new JSONArray();
+		String[] Lines = line.split("/");
+		currentKey = Lines[Lines.length - 1];
+		output.put(currentKey, currentKeyArray);
+	}
+
+	private boolean lineRegex(String line) {
+		Matcher m = p.matcher(line);
+		return m.matches();
+	}
 }
